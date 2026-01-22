cec2d5fec834f3f233dc87c8e22a0237d6d3279f
Israel <oliguer.israel1@gmail.com>
2026-01-22 14:30:53 +0800

    Create sample01.py

---
+++ sample01.py
@@ -0,0 +1,11 @@
+student_id = 2025300148
+student_name = "Israel Rodulfo L. Oliguer"
+course = "BS Information Technology"
+year_level = "1st Year"
+
+print("STUDENT INFORMATION")
+print("-------------------")
+print("Student ID :", student_id)
+print("Name       :", student_name)
+print("Course     :", course)
+print("Year Level :", year_level)
