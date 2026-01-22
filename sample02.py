8481e12988773debb81882989c5406c46f22da53
Israel <oliguer.israel1@gmail.com>
2026-01-22 15:07:50 +0800

    Create sample02.py

---
+++ sample02.py
@@ -0,0 +1,14 @@
+num1 = 69
+num2 = 420
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("BASIC ARITHMETIC OPERATION")
+print("--------------------------")
+print("Addition        :", addition)
+print("Subtraction     :", subtraction)
+print("Multiplication  :", multiplication)
+print("Division        :", division)
